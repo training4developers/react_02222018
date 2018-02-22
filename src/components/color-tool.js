025a7b2da88a7d9264833b6a7b6d395fe2863f26
Eric W. Greene <eric@ericwgreene.com>
2018-02-22 16:22:58 -0500

    added code demos

--- src/components/color-tool.js
+++ src/components/color-tool.js
@@ -4,19 +4,12 @@ export class ColorTool extends React.Component {
 
   render() {
 
-    // array of objects
-    const colors = [ 
-      { id: 1, name: 'blue', hexCode: '#0000FF' },
-      { id: 2, name: 'red', hexCode: '#FF0000' },
-      { id: 3, name: 'green', hexCode: '#00FF00' },
-    ];
-
     return <div>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>
-        {colors.map(color => <li>{color.name}</li>)}
+        {this.props.colors.map(color => <li>{color.name}</li>)}
       </ul>
     </div>;
   }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import * as ReactDOM from 'react-dom';
 
 import { ColorTool } from './components/color-tool';
 
+// array of objects
+const colorList = [ 
+  { id: 1, name: 'blue', hexCode: '#0000FF' },
+  { id: 2, name: 'red', hexCode: '#FF0000' },
+  { id: 3, name: 'green', hexCode: '#00FF00' },
+];
+
 ReactDOM.render(
   <div>
-    <ColorTool />
+    <ColorTool colors={colorList} />
   </div>,
   document.querySelector('#root'),
 );
