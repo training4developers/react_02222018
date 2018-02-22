812f61fd00a7a3fe749343c7ad7ea5f1e3f16f0d
Eric W. Greene <eric@ericwgreene.com>
2018-02-22 13:56:54 -0500

    added exercise 2

--- src/components/color-tool.js
+++ src/components/color-tool.js
@@ -3,13 +3,15 @@ import * as React from 'react';
 export class ColorTool extends React.Component {
 
   render() {
-    // return React.createElement('div', null, [
-    //   React.createElement('h1', null, 'Color Tool'),
-    //   React.createElement('span', null, 'test'),
-    // ]);
     return <div>
-      <h1>Color Tool</h1>
-      <span>test</span>
+      <header>
+        <h1>Color Tool</h1>
+      </header>
+      <ul>
+        <li>blue</li>
+        <li>red</li>
+        <li>green</li>
+      </ul>
     </div>;
   }
 }
